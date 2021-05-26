fd25da506bf7a86931f714a26cfee6d7c3bf5b56
RoyDevashis <roydeitz@gmail.com>
2021-05-26 16:32:03 +0200

    Physics_InputTest

--- Assets/BallControler.cs
+++ Assets/BallControler.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class BallControler : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public enum Material 
+    {
+    Iron,
+    Plastic,
+    Rubber
+    }
+    public Material material;
+
+    public PhysicMaterial iron;
+    public PhysicMaterial plastic;
+    public PhysicMaterial rubber;
+
+    public float massIron;
+    public float massPlastic;
+    public float massRubber;
+
+    private Rigidbody rb;
+
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        material = Material.Rubber;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+   public void MoveLeft()
+    {
+        rb.velocity = new Vector3(-2f,rb.velocity.y,rb.velocity.z);
+    }
+    public void MoveRight()
+    {
+        rb.velocity = new Vector3(2f, rb.velocity.y, rb.velocity.z);
+    }
+
 }
