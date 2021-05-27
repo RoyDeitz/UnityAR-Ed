f2c34a87b34ba7616aaf34da702bc253ab89e5bd
RoyDevashis <roydeitz@gmail.com>
2021-05-27 07:31:00 +0200

    MaterialChange_initial_Test

--- Assets/BallControler.cs
+++ Assets/BallControler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.UIElements;
 
 public class BallControler : MonoBehaviour
 {
@@ -11,15 +12,26 @@ public class BallControler : MonoBehaviour
     Plastic,
     Rubber
     }
-    public MaterialType material;
+    public MaterialType materialType;
 
+    public enum BallState 
+    {
+    BallControl,
+    CannonSet,
+    FreeFall
+    
+    }
+    public BallState ballState;
     public PhysicMaterial ironPhy;
     public PhysicMaterial plasticPhy;
     public PhysicMaterial rubberPhy;
+    private SphereCollider colliderS;
 
     public Material ironMat;
     public Material plasticMat;
     public Material rubberMat;
+    private MeshRenderer mesh;
+  
 
     public float massIron;
     public float massPlastic;
@@ -31,17 +43,29 @@ public class BallControler : MonoBehaviour
     public float force;
 
     public Transform initialPosition;
-    
+    public Transform cannonHead;
+    public GameObject btnControlGroup;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        material = MaterialType.Rubber;
+        materialType = MaterialType.Rubber;
+        ballState = BallState.BallControl;
+
+        colliderS = GetComponent<SphereCollider>();
+        mesh = GetComponent<MeshRenderer>();
+
+        colliderS.material = ironPhy;
+        mesh.material = ironMat;
+        rb.mass = massIron;
     }
 
     // Update is called once per frame
     void Update()
     {
        
+
     }
 
    public void MoveLeft()
@@ -71,6 +95,38 @@ public class BallControler : MonoBehaviour
             rb.velocity = Vector3.zero;
             this.transform.rotation = initialPosition.rotation;
             this.transform.position = initialPosition.position;
+        }
+
+        if (other.tag == "Cannon") 
+        {
+        
+        }
+    }
+
+    public void OnValueChanged(int val) 
+    {
+        if (val == 0) materialType = MaterialType.Iron;
+        else if (val == 1) materialType = MaterialType.Plastic;
+        else if (val == 2) materialType = MaterialType.Rubber;
+
+
+        if (materialType == MaterialType.Iron)
+        {
+            colliderS.material = ironPhy;
+            mesh.material = ironMat;
+            rb.mass = massIron;
+        }
+        else if (materialType == MaterialType.Plastic)
+        {
+            colliderS.material = plasticPhy;
+            mesh.material = plasticMat;
+            rb.mass = massPlastic;
+        }
+        else if (materialType == MaterialType.Rubber)
+        {
+            colliderS.material = rubberPhy;
+            mesh.material = rubberMat;
+            rb.mass = massRubber;
         }
     }
 }
