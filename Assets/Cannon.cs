4f0ffd61d45bb9a43f6e8a2eec775c1137e31b8b
RoyDevashis <roydeitz@gmail.com>
2021-05-28 08:31:56 +0200

    Explosion Added and tested

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class Cannon : MonoBehaviour
 {
     private Animator anim;
+
+    public ParticleSystem explosion;
+    
+
+    public float delay=.6f;
+    public float countdown;
+    bool isExploded = false;
     // Start is called before the first frame update
     void Start()
     {
+        isExploded = false;
         anim=GetComponent<Animator>();
+        explosion.Stop();
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        if (isExploded) 
+        {
+            if (!explosion.isPlaying)
+            {
+                explosion.Play();
+            }
+            countdown -= Time.deltaTime;
+            if (countdown <= 0) 
+            {
+                explosion.Stop();
+                isExploded = false;
+            }
+        }
+    }
     public void ShootAnim() 
     {
+        countdown = delay;
+        isExploded = true;
         anim.SetTrigger("Shoot");
     }
 }
